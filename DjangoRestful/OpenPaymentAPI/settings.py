460cc5fdd0a32d6cf144449116692a815a82d25a
Lavden <1244361371@qq.com>
2020-02-13 11:40:17 -0800

    add sstting

--- DjangoRestful/OpenPaymentAPI/settings.py
+++ DjangoRestful/OpenPaymentAPI/settings.py
@@ -88,17 +88,8 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
-    }
+    ]
 }
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
